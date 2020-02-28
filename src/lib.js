99f5bd4d5e322827bd93f9d75a624768ed21cd6a
Ubuntu <ubuntu@ip-172-31-88-93.ec2.internal>
2020-02-28 19:07:25 +0000

    deployment fixes

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@ Making calls to the server with the fetch request.
 */
 
 module.exports.getAdditionalItems = (itemId) => (
-  fetch(`http://18.208.177.170:3004/additional/${itemId}`)
+  fetch(`http://3.221.1.63:3004/additional/${itemId}`)
     .then((response) => (
       response.json()
     ))
